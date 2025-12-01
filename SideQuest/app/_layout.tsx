d87810d79e6c47310a463c6b9c27aec1b6ce414e
jackson-lafrance <jackson@codeflight.dev>
2025-12-01 10:01:46 -0500

    fix React import

--- SideQuest/app/_layout.tsx
+++ SideQuest/app/_layout.tsx
@@ -1,11 +1,10 @@
+import React, { useEffect } from "react";
 import NavigationProvider from "../core/useNavigation";
 import { Slot } from "expo-router";
-import { useEffect } from "react";
 import { initializeFirebase } from "../core/firebase";
 
 export default function RootLayout() {
   useEffect(() => {
-    // Initialize Firebase when app starts
     initializeFirebase();
   }, []);
 
